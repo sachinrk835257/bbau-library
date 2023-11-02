64306d3f81df2d0bf30c741e133478b6318adc78
sachin sen <sachinrk835257@gmail.com>
2023-11-02 19:07:57 +0530

    save

--- main/main/settings.py
+++ main/main/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://sachinrk.pythonanywhere.com/','sachinrk.pythonanywhere.com/']
 
 
 # Application definition
