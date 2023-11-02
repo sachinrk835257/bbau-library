047a959238fa0bdf570aac2129fe4f0343bddac7
sachin sen <sachinrk835257@gmail.com>
2023-11-02 18:34:10 +0530

    saved

--- main/main/settings.py
+++ main/main/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'main/static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/')
 ]
